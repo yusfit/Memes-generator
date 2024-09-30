9dcabefa3af52730168858a30def3d8d17df6372
Rampop01 <popoolarahmat96@gmail.com>
2024-09-30 10:47:53 +0100

    bu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,8 @@ export default function Header() {
   return (
     <header>
       <h1>Meme Generator</h1>
-      <a
-        className="github"
-        href="https://github.com/Oluwa-Laughter"
-        target="blank"
-      >
-        <img src="/github.png" alt="creator" />
-      </a>
+
+      <img src="/github.png" alt="creator" />
     </header>
   );
 }
